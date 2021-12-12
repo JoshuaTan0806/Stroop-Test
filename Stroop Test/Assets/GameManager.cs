1ddd0eaf32168a77eb857f3c4d23f257371bf839
Joshua Tan <joshuatan0806@gmail.com>
2021-12-12 15:59:34 +1030

    added ui stuff

--- Stroop Test/Assets/GameManager.cs
+++ Stroop Test/Assets/GameManager.cs
@@ -1,43 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
     public GameState CurrentGameState;
     public enum GameState
     {
         MainMenu,
+        Options,
         InGame,
         LoseMenu,
         ScoreMenu
     }
 
+    public Canvas MainMenuCanvas;
+    public Canvas OptionsCanvas;
+    public Canvas ScoreCanvas;
+    public Canvas LoseCanvas;
+
+    public TMP_Dropdown NumberOfColoursDropdown;
+    public int NumberOfColours;
+
+    public int Score;
+    public int Highscore;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        Highscore = PlayerPrefs.GetInt("Highscore");
         CurrentGameState = GameState.MainMenu;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
-            switch (CurrentGameState)
-            {
-                case GameState.MainMenu:
-                    CurrentGameState = GameState.InGame;
-                    break;
-
-                case GameState.LoseMenu:
-                    CurrentGameState = GameState.ScoreMenu;
-                    break;
-
-                case GameState.ScoreMenu:
-                    CurrentGameState = GameState.MainMenu;
-                    break;
-            }
+            SwitchGameState();
         }
+    }
+
+    void SwitchGameState()
+    {
+        switch (CurrentGameState)
+        {
+            case GameState.MainMenu:
+                CurrentGameState = GameState.Options;
+                SetActiveCanvas(OptionsCanvas);
+                SetActiveCanvas(MainMenuCanvas, false);
+                break;
+
+            case GameState.LoseMenu:
+                CurrentGameState = GameState.ScoreMenu;
+                SetActiveCanvas(ScoreCanvas);
+                SetActiveCanvas(LoseCanvas, false);
+                break;
+
+            case GameState.ScoreMenu:
+                CurrentGameState = GameState.MainMenu;
+                SetActiveCanvas(MainMenuCanvas);
+                SetActiveCanvas(ScoreCanvas, false);
+                break;
+        }
+    }
+
+    void SetActiveCanvas(Canvas canvas, bool setActive = true)
+    {
+        if(setActive)
+        {
+            canvas.gameObject.SetActive(true);
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+        }
+    }
+
+    public void StartGame()
+    {
+        CurrentGameState = GameState.InGame;
+        NumberOfColours = NumberOfColoursDropdown.value + 4;
+        SetActiveCanvas(OptionsCanvas, false);
     }
 }
